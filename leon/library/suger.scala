ddfa11dd69dd193bf80af951c7c4ca3a8e0060d7
ericpony <ericpony@csie.org>
2015-09-19 01:12:18 +0800

    add syntax suger for proof

---
+++ leon/library/suger.scala
@@ -0,0 +1,26 @@
+package duck
+
+import scala.language.implicitConversions
+
+package object suger {
+
+  case class LogicalOps (a: Boolean) {
+    def and (b: => Boolean) = a && b
+
+    def or (b: => Boolean) = a || b
+
+    def implies (b: => Boolean) = !a || b
+
+    def onlyif (b: => Boolean) = !b || a
+
+    def iff (b: => Boolean) = a == b
+
+    def ==> (b: => Boolean) = !a || b
+
+    def <== (b: => Boolean) = !b || a
+
+    def <==> (b: => Boolean) = a == b
+  }
+
+  implicit def extendedBoolean (a: Boolean) = LogicalOps(a)
+}
