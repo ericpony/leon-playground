2c5474f6c535797ffcb7139a6d623acbf5175b0a
ericpony <ericpony@csie.org>
2015-09-29 16:17:42 +0800

    refine heap spec

--- leon/duck/spec/heap.scala
+++ leon/duck/spec/heap.scala
@@ -1,4 +1,6 @@
 import duck.proof.sugar._
+import duck.collection._
+import duck.spec.SortedListOps.isSorted
 import leon.lang._
 import leon.annotation._
 import leon.proof._
@@ -23,6 +25,22 @@ object LeftistHeapSpec0 {
     require(isLeftistHeap(h1) && isLeftistHeap(h2) && isLeftistHeap(h3))
     heapContent(merge(h1, merge(h2, h3))) == heapContent(merge(merge(h1, h2), h3))
   } holds /* verified by Leon */
+
+  def composition_lemma (l1: List[BigInt], l2: List[BigInt]): Boolean = {
+    val L = foldl_insert(Empty, l1 ++ l2)
+    val L1 = foldl_insert(Empty, l1)
+    val L2 = foldl_insert(Empty, l2)
+    heapContent(L) == heapContent(merge(L1, L2))
+  } holds /* verified by Leon */
+
+  def foldl_insert (heap: Heap, list: List[BigInt]): Heap = {
+    require(isLeftistHeap(heap))
+    if (list == Nil[BigInt]()) heap
+    else foldl_insert(insert(list.head, heap), list.tail)
+  } ensuring { res =>
+    isLeftistHeap(res) &&
+      heapContent(res) == heapContent(heap) ++ list.content
+  } /* verified by Leon */
 }
 
 /**
@@ -37,48 +55,52 @@ object LeftistHeapOps {
 
   sealed abstract class Heap
 
-  case object Leaf extends Heap
+  case object Empty extends Heap
 
   case class Node (rk: BigInt, value: BigInt, left: Heap, right: Heap) extends Heap
 
   ////////// Interface functions ////////////
 
-  def findMax (h: Heap): Option[BigInt] = {
+  def findMin (h: Heap): Option[BigInt] = {
     require(isLeftistHeap(h))
     h match {
-      case Leaf             => None[BigInt]()
+      case Empty            => None[BigInt]()
       case Node(_, v, _, _) => Some(v)
     }
-  } ensuring { res => h != Leaf implies
+  } ensuring { res => h != Empty implies
     res.isDefined && {
       val Node(_, _, left, right) = h
       val m = res.get
-      m >= max(findMax(left).getOrElse(m), findMax(right).getOrElse(m))
+      m <= min(findMin(left).getOrElse(m), findMin(right).getOrElse(m)) && {
+        h != Empty implies
+          heapContent(h) == heapContent(deleteMin(h)) ++ Set(m)
+      }
     }
   } /* verified by Leon */
 
-  def removeMax (h: Heap): Heap = {
+  def deleteMin (h: Heap): Heap = {
     require(isLeftistHeap(h))
     h match {
       case Node(_, _, l, r) => merge(l, r)
-      case l@Leaf           => l
+      case l@Empty          => l
     }
   } ensuring { res =>
     size(res) >= size(h) - 1 &&
       heapContent(res).subsetOf(heapContent(h)) && {
-      h != Leaf implies
-        heapContent(h) == heapContent(res) ++ Set(findMax(h).get)
+      h != Empty && res != Empty implies
+        findMin(h).get <= findMin(res).get &&
+          heapContent(h) == heapContent(res) ++ Set(findMin(h).get)
     }
   } /* verified by Leon */
 
   def merge (h1: Heap, h2: Heap): Heap = {
     require(isLeftistHeap(h1) && isLeftistHeap(h2))
     h1 match {
-      case Leaf                => h2
+      case Empty               => h2
       case Node(_, v1, l1, r1) => h2 match {
-        case Leaf                => h1
+        case Empty               => h1
         case Node(_, v2, l2, r2) =>
-          if (v1 > v2)
+          if (v1 <= v2)
             makeT(v1, l1, merge(r1, h2))
           else
             makeT(v2, l2, merge(h1, r2))
@@ -88,16 +110,16 @@ object LeftistHeapOps {
     isLeftistHeap(res) &&
       size(res) == size(h1) + size(h2) &&
       heapContent(res) == heapContent(h1) ++ heapContent(h2) && {
-      res != Leaf implies {
-        val v = findMax(res).get
-        v == max(findMax(h1).getOrElse(v), findMax(h2).getOrElse(v))
+      res != Empty implies {
+        val v = findMin(res).get
+        v == min(findMin(h1).getOrElse(v), findMin(h2).getOrElse(v))
       }
     }
   } /* verified by Leon */
 
   def insert (element: BigInt, heap: Heap): Heap = {
     require(isLeftistHeap(heap))
-    merge(Node(1, element, Leaf, Leaf), heap)
+    merge(Node(1, element, Empty, Empty), heap)
   } ensuring { res => isLeftistHeap(res) &&
     size(res) == size(heap) + 1 &&
     heapContent(res) == heapContent(heap) ++ Set(element)
@@ -105,27 +127,36 @@ object LeftistHeapOps {
 
   def isLeftistHeap (h: Heap): Boolean = {
     h match {
-      case Leaf                    => true
+      case Empty                   => true
       case Node(_, v, left, right) =>
         isLeftistHeap(left) && isLeftistHeap(right) &&
           rightHeight(left) >= rightHeight(right) &&
           isPartiallyOrdered(v, left, right)
     }
+  }
 
-    def toSortedList(h: Heap): List[BigInt] = ???
+  @induct
+  def toSortedList (h: Heap): List[BigInt] = {
+    require(isLeftistHeap(h))
+    h match {
+      case Empty => Nil[BigInt]()
+      case _     => findMin(h).get :: toSortedList(deleteMin(h))
+    }
+  } ensuring { res =>
+    isSorted(res) && res.size == size(h)
   }
 
   def isPartiallyOrdered (value: BigInt, h1: Heap, h2: Heap) = {
     require(isLeftistHeap(h1) && isLeftistHeap(h2))
     h1 match {
-      case Leaf              => h2 match {
-        case Leaf              => true
-        case Node(_, v2, _, _) => value >= v2
+      case Empty             => h2 match {
+        case Empty             => true
+        case Node(_, v2, _, _) => value <= v2
       }
-      case Node(_, v1, _, _) => value >= v1 && {
+      case Node(_, v1, _, _) => value <= v1 && {
         h2 match {
-          case Leaf              => true
-          case Node(_, v2, _, _) => value >= v2
+          case Empty             => true
+          case Node(_, v2, _, _) => value <= v2
         }
       }
     }
@@ -133,7 +164,7 @@ object LeftistHeapOps {
 
   def size (t: Heap): BigInt = {
     t match {
-      case Leaf             => BigInt(0)
+      case Empty            => BigInt(0)
       case Node(_, v, l, r) => size(l) + 1 + size(r)
     }
   }
@@ -141,19 +172,19 @@ object LeftistHeapOps {
   ////////// Abstraction functions ////////////
 
   def heapContent (h: Heap): Set[BigInt] = h match {
-    case Leaf                    => Set.empty[BigInt]
+    case Empty                   => Set.empty[BigInt]
     case Node(_, v, left, right) => Set(v) ++ heapContent(left) ++ heapContent(right)
   }
 
   ///////////// Helper functions //////////////
 
-  private def max (a: BigInt, b: BigInt) = {
-    if (a > b) a else b
+  private def min (a: BigInt, b: BigInt) = {
+    if (a <= b) a else b
   } ensuring { res => res == a || res == b }
 
   private def rightHeight (h: Heap): BigInt = {
     h match {
-      case Leaf             => BigInt(0)
+      case Empty            => BigInt(0)
       case Node(_, _, _, r) => rightHeight(r) + 1
     }
   } ensuring (_ >= 0)
