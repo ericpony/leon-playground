accb6ab5e2e4ff321796c11141974f4ab36d76ab
ericpony <ericpony@csie.org>
2015-10-03 02:54:12 +0800

    strengthen the post-condition of union

--- leon/duck/collection/distinct-list-set.scala
+++ leon/duck/collection/distinct-list-set.scala
@@ -7,30 +7,33 @@ import leon.lang._
 import leon.annotation._
 
 package object DistinctListSetOps {
-  def insert[V] (set : List[V], e : V) : List[V] = {
-    if (set.isEmpty) Cons (e, Nil[V])
-    else if (set.head == e) set 
-    else set.head::insert (set.tail, e)
+  def insert[V] (set: List[V], e: V): List[V] = {
+    if (set.isEmpty) Cons(e, Nil[V])
+    else if (set.head == e) set
+    else set.head :: insert(set.tail, e)
   } ensuring {
-    res => res.content == set.content ++ Set (e) &&
-           res.size <= set.size + 1
+    res => res.content == set.content ++ Set(e) &&
+      res.size <= set.size + 1
   }
 
-  def remove[V] (set : List[V], e : V) : List[V] = {
+  def remove[V] (set: List[V], e: V): List[V] = {
     if (set.isEmpty) set
-    else if (set.head == e) set.tail 
-    else set.head::remove (set.tail, e)
+    else if (set.head == e) set.tail
+    else set.head :: remove(set.tail, e)
   } ensuring {
-    res => res.content.subsetOf (set.content) &&
-           res.size <= set.size
+    res => res.content.subsetOf(set.content) &&
+      res.size <= set.size
   }
 
-  def union[V] (s : List[V], t : List[V]) : List[V] = {
-    s.foldRight (t) ((e, tt) => insert (tt, e))
+  def union[V] (s: List[V], t: List[V]): List[V] = {
+    //    t match {
+    //      case Nil()      => s
+    //      case Cons(h, t) => union(insert(s, h), t)
+    //    }
+    s.foldRight(t)((e, tt) => insert(tt, e))
   } ensuring {
-    res => res.content == s.content ++ t.content because 
-             union_content (s, t) &&
-           res.size >= t.size &&
-           res.size <= s.size + t.size
+    res => res == union(t, s) &&
+      res.content == s.content ++ t.content &&
+      res.size == s.size + t.size - (s & t).size
   }
 }
