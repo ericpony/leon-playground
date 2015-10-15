eae85dae972cfa24bc5c335fe90e1ce70b2da90f
ericpony <ericpony@csie.org>
2015-10-15 17:31:02 +0800

    format code

--- leon/duck/spec/heap.scala
+++ leon/duck/spec/heap.scala
@@ -19,53 +19,45 @@ import LeftistHeapLemmas1._
  */
 object LeftistHeapSpec1 {
 
-  case class Bisimulation(h1: Heap) {
-    def ~(h2: Heap): Boolean = permutation(h1.toList, h2.toList)
-  }
-
-  implicit def bisimulate(h: Heap) = Bisimulation(h)
-
-  implicit def toList(h: Heap) = h.toList
-
   @library
-  def insert_invariant(h1: Heap, h2: Heap, e: BigInt): Boolean = {
+  def insert_invariant (h1: Heap, h2: Heap, e: BigInt): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap && h1 ~ h2)
     h1.insert(e) ~ h2.insert(e) because {
-      val l1 = h1.insert(e).toList
-      val l2 = h2.insert(e).toList
-      permutation(l1, e :: h1.toList) &&
-        permutation(e :: h2.toList, l2) &&
-        //permutation(e :: h1.toList, e :: h2.toList) because
-        permutation_cons(h1.toList, h2.toList, e) &&
-        permutation_tran_lemma(l1, e :: h1.toList, e :: h2.toList) &&
-        permutation_tran_lemma(l1, e :: h2.toList, l2)
+      val l1 = h1.insert(e)
+      val l2 = h2.insert(e)
+      permutation(l1, e :: h1) &&
+        permutation(e :: h2, l2) &&
+        //permutation(e :: h1, e :: h2) because
+        permutation_cons(h1, h2, e) &&
+        permutation_tran_lemma(l1, e :: h1, e :: h2) &&
+        permutation_tran_lemma(l1, e :: h2, l2)
     }
   } holds /* verified by Leon */
 
   @library
-  def findMin_invariant(h1: Heap, h2: Heap): Boolean = {
+  def findMin_invariant (h1: Heap, h2: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap && h1 ~ h2)
     h1.findMin == h2.findMin
   } holds /* verified by Leon */
 
   @library
-  def merge_invariant(h1: Heap, h2: Heap, h3: Heap): Boolean = {
+  def merge_invariant (h1: Heap, h2: Heap, h3: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap && h3.isLeftistHeap && h1 ~ h2)
     h1.merge(h3) ~ h2.merge(h3) because {
-      val l1 = h1.merge(h3).toList
-      val l2 = h2.merge(h3).toList
-      // permutation(l1, h1.toList ++ h3.toList) because
+      val l1 = h1.merge(h3)
+      val l2 = h2.merge(h3)
+      // permutation(l1, h1 ++ h3) because
       merge_concat_lemma(h1, h3) &&
-        // permutation(l2, h2.toList ++ h3.toList) because
+        // permutation(l2, h2 ++ h3) because
         merge_concat_lemma(h2, h3) &&
-        // permutation(h1.toList ++ h3.toList, h2.toList ++ h3.toList) because
-        permutation_append(h1.toList, h2.toList, h3.toList) &&
-        permutation_tran(l1, h1.toList ++ h3.toList, h2.toList ++ h3.toList) &&
-        permutation_tran(l1, h2.toList ++ h3.toList, l2)
+        // permutation(h1 ++ h3, h2 ++ h3) because
+        permutation_append(h1, h2, h3) &&
+        permutation_tran(l1, h1 ++ h3, h2 ++ h3) &&
+        permutation_tran(l1, h2 ++ h3, l2)
     }
   } holds /* verified by Leon */
 
-  def insert_commu_prop(h: Heap, e1: BigInt, e2: BigInt): Boolean = {
+  def insert_commu_prop (h: Heap, e1: BigInt, e2: BigInt): Boolean = {
     require(h.isLeftistHeap)
     h.insert(e1).insert(e2) ~ h.insert(e2).insert(e1) because {
       //permutation(h.insert(e1), e1 :: h) because
@@ -83,58 +75,58 @@ object LeftistHeapSpec1 {
     }
   } holds /* verified by Leon */
 
-  def merge_commu_prop(h1: Heap, h2: Heap): Boolean = {
+  def merge_commu_prop (h1: Heap, h2: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap)
     h1.merge(h2) ~ h2.merge(h1)
   } holds /* timeout */
 
-  def merge_assoc_prop(h1: Heap, h2: Heap, h3: Heap): Boolean = {
+  def merge_assoc_prop (h1: Heap, h2: Heap, h3: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap && h3.isLeftistHeap)
     h1.merge(h2).merge(h3) ~ h1.merge(h2.merge(h3))
   } holds /* timeout */
 
-  def composition_prop(l1: List[BigInt], l2: List[BigInt]): Boolean = {
+  def composition_prop (l1: List[BigInt], l2: List[BigInt]): Boolean = {
     val L = foldl_insert(Empty(), l1 ++ l2)
     val L1 = foldl_insert(Empty(), l1)
     val L2 = foldl_insert(Empty(), l2)
     L ~ L1.merge(L2)
   } holds /* timeout */
 
-  def foldl_insert(heap: Heap, list: List[BigInt]): Heap = {
+  def foldl_insert (heap: Heap, list: List[BigInt]): Heap = {
     require(heap.isLeftistHeap)
     if (list == Nil[BigInt]()) heap
     else foldl_insert(heap.insert(list.head), list.tail)
   } ensuring { res =>
     res.isLeftistHeap &&
-      permutation(res.toList, heap.toList ++ list)
+      permutation(res, heap ++ list)
   }
 }
 
 @library
 object LeftistHeapSpec0 {
-  def insert_commu_prop(h: Heap, e1: BigInt, e2: BigInt): Boolean = {
+  def insert_commu_prop (h: Heap, e1: BigInt, e2: BigInt): Boolean = {
     require(h.isLeftistHeap)
     h.insert(e1).insert(e2).content == h.insert(e2).insert(e1).content
   } holds /* verified by Leon */
 
-  def merge_commu_prop(h1: Heap, h2: Heap): Boolean = {
+  def merge_commu_prop (h1: Heap, h2: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap)
     h1.merge(h2).content == h2.merge(h1).content
   } holds /* verified by Leon */
 
-  def merge_assoc_prop(h1: Heap, h2: Heap, h3: Heap): Boolean = {
+  def merge_assoc_prop (h1: Heap, h2: Heap, h3: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap && h3.isLeftistHeap)
     h1.merge(h2).merge(h3).content == h1.merge(h2.merge(h3)).content
   } holds /* verified by Leon */
 
-  def composition_prop(l1: List[BigInt], l2: List[BigInt]): Boolean = {
+  def composition_prop (l1: List[BigInt], l2: List[BigInt]): Boolean = {
     val L = foldl_insert(Empty(), l1 ++ l2)
     val L1 = foldl_insert(Empty(), l1)
     val L2 = foldl_insert(Empty(), l2)
     L.content == L1.merge(L2).content
   } holds /* verified by Leon */
 
-  def foldl_insert(heap: Heap, list: List[BigInt]): Heap = {
+  def foldl_insert (heap: Heap, list: List[BigInt]): Heap = {
     require(heap.isLeftistHeap)
     if (list == Nil[BigInt]()) heap
     else foldl_insert(heap.insert(list.head), list.tail)
@@ -158,7 +150,7 @@ sealed abstract class Heap {
   def findMin: Option[BigInt] = {
     require(this.isLeftistHeap)
     this match {
-      case Empty() => None[BigInt]()
+      case Empty()          => None[BigInt]()
       case Node(_, v, _, _) => Some(v)
     }
   } ensuring { res => this != Empty() implies
@@ -176,7 +168,7 @@ sealed abstract class Heap {
     require(this.isLeftistHeap)
     this match {
       case Node(_, _, l, r) => l.merge(r)
-      case l@Empty() => l
+      case l@Empty()        => l
     }
   } ensuring { res =>
     res.size >= this.size - 1 &&
@@ -187,12 +179,12 @@ sealed abstract class Heap {
     }
   } /* verified by Leon */
 
-  def merge(h: Heap): Heap = {
+  def merge (h: Heap): Heap = {
     require(this.isLeftistHeap && h.isLeftistHeap)
     this match {
-      case Empty() => h
+      case Empty()             => h
       case Node(_, v1, l1, r1) => h match {
-        case Empty() => this
+        case Empty()             => this
         case Node(_, v2, l2, r2) =>
           if (v1 <= v2)
             makeT(v1, l1, r1.merge(h))
@@ -207,16 +199,11 @@ sealed abstract class Heap {
       res != Empty() implies {
         val v = res.findMin.get
         v == min(this.findMin.getOrElse(v), h.findMin.getOrElse(v))
-      } && {
-        this != Empty() implies {
-          val Node(_, v, l, r) = res
-          permutation(res.toList, v :: (l.toList ++ r.toList))
-        }
       }
     }
   } /* verified by Leon */
 
-  def insert(element: BigInt): Heap = {
+  def insert (element: BigInt): Heap = {
     require(this.isLeftistHeap)
     this.merge(Node(1, element, Empty(), Empty()))
     // Node(1, element, Empty(), Empty()).merge(this)
@@ -227,7 +214,7 @@ sealed abstract class Heap {
   } /* verified by Leon */
 
   def isLeftistHeap: Boolean = this match {
-    case Empty() => true
+    case Empty()                 => true
     case Node(_, v, left, right) =>
       left.isLeftistHeap && right.isLeftistHeap &&
         rightHeight(left) >= rightHeight(right) &&
@@ -236,7 +223,7 @@ sealed abstract class Heap {
 
   def toList: List[BigInt] = {
     this match {
-      case Empty() => Nil[BigInt]()
+      case Empty()          => Nil[BigInt]()
       case Node(_, v, l, r) => v :: (l.toList ++ r.toList)
     }
   } ensuring { res =>
@@ -244,7 +231,7 @@ sealed abstract class Heap {
       res.content == this.content &&
       (this != Empty() implies {
         val Node(_, v, l, r) = this
-        permutation(this.toList, v :: (l.toList ++ r.toList))
+        permutation(this, v :: (l ++ r))
       })
   }
 
@@ -253,7 +240,7 @@ sealed abstract class Heap {
     require(this.isLeftistHeap)
     this match {
       case Empty() => Nil[BigInt]()
-      case _ => this.findMin.get :: this.deleteMin.toSortedList
+      case _       => this.findMin.get :: this.deleteMin.toSortedList
     }
   } ensuring { res =>
     isSorted(res) && res.size == this.size
@@ -261,7 +248,7 @@ sealed abstract class Heap {
 
   def size: BigInt = {
     this match {
-      case Empty() => BigInt(0)
+      case Empty()          => BigInt(0)
       case Node(_, v, l, r) => l.size + 1 + r.size
     }
   }
@@ -270,28 +257,37 @@ sealed abstract class Heap {
 
   def content: Set[BigInt] =
     this match {
-      case Empty() => Set.empty[BigInt]
+      case Empty()                 => Set.empty[BigInt]
       case Node(_, v, left, right) => Set(v) ++ left.content ++ right.content
     }
 
   ///////////// Helper functions //////////////
 
-  private def min(a: BigInt, b: BigInt) = {
+  private def min (a: BigInt, b: BigInt) = {
     if (a <= b) a else b
   } ensuring { res => res == a || res == b }
+}
 
-  //  def ~ (h2: Heap): Boolean = permutation(this.toList, h2.toList)
+object Heap {
+
+  case class Bisimulation (h1: Heap) {
+    def ~ (h2: Heap): Boolean = permutation(h1, h2)
+  }
+
+  implicit def bisimulate (h: Heap): Bisimulation = Bisimulation(h)
+
+  implicit def toList (h: Heap): List[BigInt] = h.toList
 }
 
 object LeftistHeapOps {
-  def rightHeight(h: Heap): BigInt = {
+  def rightHeight (h: Heap): BigInt = {
     h match {
-      case Empty() => BigInt(0)
+      case Empty()          => BigInt(0)
       case Node(_, _, _, r) => rightHeight(r) + 1
     }
   } ensuring (_ >= 0)
 
-  def makeT(v: BigInt, l: Heap, r: Heap): Heap = {
+  def makeT (v: BigInt, l: Heap, r: Heap): Heap = {
     require(l.isLeftistHeap && r.isLeftistHeap &&
       isPartiallyOrdered(v, l, r))
     if (rightHeight(l) >= rightHeight(r))
@@ -300,19 +296,19 @@ object LeftistHeapOps {
       Node(rightHeight(l) + 1, v, r, l)
   } ensuring { res =>
     res.isLeftistHeap &&
-      permutation(res.toList, v :: (l.toList ++ r.toList))
+      permutation(res, v :: (l ++ r))
   } /* verified by Leon */
 
-  def isPartiallyOrdered(value: BigInt, h1: Heap, h2: Heap) = {
+  def isPartiallyOrdered (value: BigInt, h1: Heap, h2: Heap) = {
     require(h1.isLeftistHeap && h2.isLeftistHeap)
     h1 match {
-      case Empty() => h2 match {
-        case Empty() => true
+      case Empty()           => h2 match {
+        case Empty()           => true
         case Node(_, v2, _, _) => value <= v2
       }
       case Node(_, v1, _, _) => value <= v1 && {
         h2 match {
-          case Empty() => true
+          case Empty()           => true
           case Node(_, v2, _, _) => value <= v2
         }
       }
@@ -321,36 +317,28 @@ object LeftistHeapOps {
 
 }
 
-case class Empty() extends Heap
+case class Empty () extends Heap
 
-case class Node(rk: BigInt, value: BigInt, left: Heap, right: Heap) extends Heap
+case class Node (rk: BigInt, value: BigInt, left: Heap, right: Heap) extends Heap
 
 object LeftistHeapLemmas1 {
 
-  case class Bisimulation(h1: Heap) {
-    def ~(h2: Heap): Boolean = permutation(h1.toList, h2.toList)
-  }
-
-  implicit def bisimulate(h: Heap): Bisimulation = Bisimulation(h)
-
-  implicit def toList(h: Heap): List[BigInt] = h.toList
-
-  def List(e: BigInt) = Cons(e, Nil[BigInt]())
+  def List (e: BigInt) = Cons(e, Nil[BigInt]())
 
   @induct
-  def list_decomp(l1: List[BigInt], l2: List[BigInt], e: BigInt) = {
+  def list_decomp (l1: List[BigInt], l2: List[BigInt], e: BigInt) = {
     l1 ++ (e :: l2) == (l1 ++ List(e)) ++ l2
   } holds
 
   @induct
-  def permutation_prefix_comm(a: BigInt, b: BigInt, l: List[BigInt]) = {
+  def permutation_prefix_comm (a: BigInt, b: BigInt, l: List[BigInt]) = {
     permutation(a :: b :: l, b :: a :: l)
   } holds
 
   /**
    * WARNING: Need ~20 sec. to be verified on my desktop
    */
-  def insert_cons_lemma(h: Heap, e: BigInt): Boolean = {
+  def insert_cons_lemma (h: Heap, e: BigInt): Boolean = {
     require(h.isLeftistHeap)
     permutation(h.insert(e), e :: h) because {
       if (h == Empty()) trivial
@@ -397,13 +385,13 @@ object LeftistHeapLemmas1 {
   /**
    * Warning: Need ~20 sec. to be verified on my desktop
    */
-  def merge_concat_lemma(h1: Heap, h2: Heap): Boolean = {
+  def merge_concat_lemma (h1: Heap, h2: Heap): Boolean = {
     require(h1.isLeftistHeap && h2.isLeftistHeap)
     permutation(h1.merge(h2), h1 ++ h2) because {
       h1 match {
-        case Empty() => permutation_refl(h2)
+        case Empty()             => permutation_refl(h2)
         case Node(_, v1, l1, r1) => h2 match {
-          case Empty() => permutation_refl(h1)
+          case Empty()             => permutation_refl(h1)
           case Node(_, v2, l2, r2) =>
             val h = h1.merge(h2)
             if (v1 <= v2) {
