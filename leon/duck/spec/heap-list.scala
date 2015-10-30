e5589c2d2c4f530cb5966198e437c5654c1bd0b8
ericpony <ericpony@csie.org>
2015-10-30 23:25:24 +0800

    refactor heap data types

--- leon/duck/spec/heap-list.scala
+++ leon/duck/spec/heap-list.scala
@@ -16,15 +16,17 @@ import scala.language.postfixOps
 import scala.language.implicitConversions
 
 /**
- * Bisimulation between LeftistHeap and List
+ * Bisimulation between LeftistHeap and ListHeap
  */
 object Heap_List_BisimulationSpec {
-  implicit def asHeap (l: List[BigInt]) = HeapADT(l)
+  implicit def toList (h: ListHeap) = h.toList
+
+  implicit def toHeap (l: List[BigInt]) = ListHeap(l)
 
   /**
    * h ~ l implies h.insert(e) ~ l.insert(e)
    */
-  def bisim_insert (h: Heap, l: List[BigInt], e: BigInt): Boolean = {
+  def bisim_insert (h: LeftistHeap, l: ListHeap, e: BigInt): Boolean = {
     require(h.isHeap && h ~ l)
     h.insert(e) ~ l.insert(e) because {
       val L1 = h.insert(e)
@@ -40,7 +42,7 @@ object Heap_List_BisimulationSpec {
   /**
    * h1 ~ l1 and h2 ~ l2 implies h1.merge(h2) ~ l1.merge(l2)
    */
-  def bisim_merge (h1: Heap, h2: Heap, l1: List[BigInt], l2: List[BigInt]): Boolean = {
+  def bisim_merge (h1: LeftistHeap, h2: LeftistHeap, l1: ListHeap, l2: ListHeap): Boolean = {
     require(h1.isHeap && h2.isHeap && h1 ~ l1 && h2 ~ l2)
     h1.merge(h2) ~ l1.merge(l2) because {
       val L1 = h1.merge(h2)
@@ -56,10 +58,10 @@ object Heap_List_BisimulationSpec {
   /**
    * h ~ l implies h.findMin == l.findMin
    */
-  def bisim_findMin (h: Heap, l: List[BigInt]): Boolean = {
+  def bisim_findMin (h: LeftistHeap, l: ListHeap): Boolean = {
     require(h.isHeap && h ~ l)
     h.findMin == l.findMin because {
-      if (h == Empty())
+      if (h.isEmpty)
         trivial
       else {
         findMin_lemma(h) &&
@@ -71,10 +73,10 @@ object Heap_List_BisimulationSpec {
   /**
    * h ~ l implies h.deleteMin ~ l.deleteMin
    */
-  def bisim_deleteMin (h: Heap, l: List[BigInt]): Boolean = {
+  def bisim_deleteMin (h: LeftistHeap, l: ListHeap): Boolean = {
     require(h.isHeap && h ~ l)
     h.deleteMin ~ l.deleteMin because {
-      if (h == Empty())
+      if (h.isEmpty)
         trivial
       else {
         bisim_deleteMin(h, l) &&
@@ -86,77 +88,83 @@ object Heap_List_BisimulationSpec {
   /**
    * h1 ~ h2 implies h1.size == h2.size
    */
-  def bisim_size (h: Heap, l: List[BigInt]): Boolean = {
+  def bisim_size (h: LeftistHeap, l: ListHeap): Boolean = {
     require(h.isHeap && h ~ l)
     h.size == l.size
   } holds /* verified by Leon */
 
   @induct
-  def list_insert_comm_prop (l: List[BigInt], e1: BigInt, e2: BigInt) = {
+  def list_insert_comm (l: List[BigInt], e1: BigInt, e2: BigInt) = {
     l.insert(e1).insert(e2) ~ l.insert(e2).insert(e1)
   } holds /* verified by Leon */
 
-  def list_merge_comm_prop (l1: List[BigInt], l2: List[BigInt]): Boolean = {
+  def insert_comm_prop (l: ListHeap, e1: BigInt, e2: BigInt) = {
+    l.insert(e1).insert(e2) ~ l.insert(e2).insert(e1) because
+      list_insert_comm(l, e1, e2)
+  } holds /* verified by Leon */
+
+  def merge_comm_prop (l1: ListHeap, l2: ListHeap): Boolean = {
     l1.merge(l2) ~ l2.merge(l1) because
       permutation_concat_comm(l1, l2)
   } holds /* verified by Leon */
 
-  def list_merge_assoc_prop (l1: List[BigInt], l2: List[BigInt], l3: List[BigInt]): Boolean = {
+  def merge_assoc_prop (l1: ListHeap, l2: ListHeap, l3: ListHeap): Boolean = {
     l1.merge(l2).merge(l3) ~ l1.merge(l2.merge(l3)) because
       permutation_concat_assoc(l1, l2, l3)
   } holds /* verified by Leon */
 
-  /**
-   * h.insert(e1).insert(e2) ~ h.insert(e2).insert(e1)
-   */
-  def heap_insert_comm_prop (h: Heap, e1: BigInt, e2: BigInt): Boolean = {
-    require(h.isHeap)
-    h.insert(e1).insert(e2) ~ h.insert(e2).insert(e1) because {
-      bisim_insert(h, h, e1) &&
-        bisim_insert(h.insert(e1), e1 :: h, e2) &&
-        list_insert_comm_prop(h, e1, e2) &&
-        permutation_tran(
-          h.insert(e1).insert(e2),
-          e2 :: e1 :: h,
-          e1 :: e2 :: h
-        ) &&
-        heap_insert_lemma(h, e2) &&
-        permutation_cons(h.insert(e2), e2 :: h, e1) &&
-        heap_insert_lemma(h.insert(e2), e1) &&
-        permutation_tran(
-          h.insert(e2).insert(e1),
-          e1 :: h.insert(e2),
-          e1 :: e2 :: h
-        ) &&
-        permutation_tran(
-          h.insert(e1).insert(e2),
-          e1 :: e2 :: h,
-          h.insert(e2).insert(e1)
-        )
-    }
-  } holds /* verified by Leon */
-
-  /**
-   * h1.merge(h2) ~ h2.merge(h1)
-   */
-  def heap_merge_comm_prop (h1: Heap, h2: Heap): Boolean = {
-    require(h1.isHeap && h2.isHeap)
-    h1.merge(h2) ~ h2.merge(h1) because {
-      bisim_merge(h1, h2, h1, h2) &&
-        bisim_merge(h2, h1, h2, h1) &&
-        permutation_concat_comm(h1, h2) &&
-        permutation_tran(h1.merge(h2), h1 ++ h2, h2 ++ h1) &&
-        // The following line is weird. It is not needed in logic,
-        // but Leon cannot prove this lemma without it.
-        permutation(h2 ++ h1, h2.merge(h1)) && // WEIRD!!
-        permutation_tran(h1.merge(h2), h2 ++ h1, h2.merge(h1))
-    }
-  } holds /* verified by Leon */
+  //  /**
+  //   * h.insert(e1).insert(e2) ~ h.insert(e2).insert(e1)
+  //   */
+  //  def heap_insert_comm_prop (h: ListHeap, e1: BigInt, e2: BigInt): Boolean = {
+  //    require(h.isHeap)
+  //    h.insert(e1).insert(e2) ~ h.insert(e2).insert(e1) because {
+  //      bisim_insert(h, h, e1) &&
+  //        bisim_insert(h.insert(e1), e1 :: h, e2) &&
+  //        list_insert_comm_prop(h, e1, e2) &&
+  //        permutation_tran(
+  //          h.insert(e1).insert(e2),
+  //          e2 :: e1 :: h,
+  //          e1 :: e2 :: h
+  //        ) &&
+  //        heap_insert_lemma(h, e2) &&
+  //        permutation_cons(h.insert(e2), e2 :: h, e1) &&
+  //        heap_insert_lemma(h.insert(e2), e1) &&
+  //        permutation_tran(
+  //          h.insert(e2).insert(e1),
+  //          e1 :: h.insert(e2),
+  //          e1 :: e2 :: h
+  //        ) &&
+  //        permutation_tran(
+  //          h.insert(e1).insert(e2),
+  //          e1 :: e2 :: h,
+  //          h.insert(e2).insert(e1)
+  //        )
+  //    }
+  //  } holds /* verified by Leon */
+  //
+  //  /**
+  //   * h1.merge(h2) ~ h2.merge(h1)
+  //   */
+  //  def heap_merge_comm_prop (h1: ListHeap, h2: ListHeap): Boolean = {
+  //    require(h1.isHeap && h2.isHeap)
+  //    h1.merge(h2) ~ h2.merge(h1) because {
+  //      bisim_merge(h1, h2, h1, h2) &&
+  //        bisim_merge(h2, h1, h2, h1) &&
+  //        permutation_concat_comm(h1, h2) &&
+  //        permutation_tran(h1.merge(h2), h1 ++ h2, h2 ++ h1) &&
+  //        // The following line is weird. It is not needed in logic,
+  //        // but Leon cannot prove this lemma without it.
+  //        permutation(h2 ++ h1, h2.merge(h1)) && // WEIRD!!
+  //        permutation_tran(h1.merge(h2), h2 ++ h1, h2.merge(h1))
+  //    }
+  //  } holds /* verified by Leon */
 }
 
-case class HeapADT (list: List[BigInt]) {
+//case class ListHeap (list: List[BigInt]) extends HeapADT {
+case class ListHeap (list: List[BigInt]) {
 
-  implicit def asHeap (l: List[BigInt]) = HeapADT(l)
+  implicit def toList (h: ListHeap): List[BigInt] = h.toList
 
   def ~ (l: List[BigInt]) = permutation(list, l)
 
@@ -167,16 +175,24 @@ case class HeapADT (list: List[BigInt]) {
       Some(min(list))
   }
 
-  def deleteMin: List[BigInt] = {
+  def deleteMin: ListHeap = {
     if (list == Nil[BigInt]())
-      list
+      ListHeap(list)
     else
-      delete(list, list.findMin.get)
+      ListHeap(delete(list, findMin.get))
   }
 
-  def merge (l: List[BigInt]): List[BigInt] = list ++ l
+  def merge (l: ListHeap): ListHeap = ListHeap(list ++ l)
 
-  def insert (e: BigInt): List[BigInt] = e :: list
+  def insert (e: BigInt): ListHeap = ListHeap(e :: list)
 
   def toSortedList: List[BigInt] = sort(list)
+
+  def content = list.content
+
+  def toList: List[BigInt] = list
+
+  def isHeap: Boolean = true
+
+  def size: BigInt = list.size
 }
