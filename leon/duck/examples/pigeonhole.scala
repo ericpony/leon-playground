eaa224ac36e7a365ece6ca4c9a45de369b7f160a
ericpony <ericpony@csie.org>
2016-02-25 02:34:03 +0800

    simplify proof and refactor code

--- leon/duck/examples/pigeonhole.scala
+++ leon/duck/examples/pigeonhole.scala
@@ -10,50 +10,48 @@ object PigeonHolePrinciples {
 
   def sum (a : List[BigInt]) : BigInt = a.foldRight(BigInt(0))(_ + _)
 
-  def hasCollision (a : List[BigInt]) : Boolean = {
+  def has_collision (a : List[BigInt]) : Boolean = {
     a match {
       case Nil()  => false
-      case h :: t => t.contains(h) || hasCollision(t)
+      case h :: t => t.contains(h) || has_collision(t)
     }
   }
 
-  def pigeonhole_principle (a : List[BigInt], n : BigInt) : Boolean = {
-    require(n == a.length && a.length >= 2 && boundedBy(a, 0, n))
-    hasCollision(a) because {
-      if (a.length == 2) {
+  def pigeonhole_principle (a : List[BigInt]) : Boolean = {
+    require(a.length >= 2 && bounded_by(a, 0, a.length))
+    has_collision(a) because {
+      if (a.length == BigInt(2)) {
         trivial
       } else {
-        if (count(a, n - 1) >= 2) {
-          pigeon_lemma_1(a, n - 1)
-        } else if (count(a, n - 1) == 0) {
-          pigeon_lemma_3(a, n - 1) &&
-            pigeon_lemma_2(a, n) &&
-            pigeonhole_principle(a.tail, n - 1) &&
-            pigeon_lemma_5(a, a.head, n)
-        } else {
-          // count(a, n - 1) == 1
-          val a2 = a.removeFirst(n - 1)
-          pigeon_lemma_4(a, n) &&
-            pigeonhole_principle(a2, n - 1) &&
-            pigeon_lemma_5(a, n - 1, n)
+        frequency_of(a, a.length - 1) match {
+          case BigInt(1) =>
+            pigeon_lemma_2(a, a.length) &&
+              pigeon_lemma_3(a, a.length - 1, a.length) &&
+              pigeonhole_principle(a.removeFirst(a.length - 1))
+          case BigInt(0) =>
+            can_tighten_upperbound(a, 0, a.length) &&
+              pigeon_lemma_3(a, a.head, a.length) &&
+              pigeonhole_principle(a.tail)
+          case _         =>
+            pigeon_lemma_1(a, a.length - 1)
         }
       }
     }
   } holds /* verified */
 
-  def pigeonhole_principle_array (a : Array[Int]) : Boolean = {
+  def pigeonhole_principle (a : Array[Int]) : Boolean = {
     require(forall((i : Int) =>
-      ArrayOps(a).isDefinedAt(i) ==> boundedBy(a(i), 0, a.length))
+      ArrayOps(a).is_defined_at(i) ==> bounded_by(a(i), 0, a.length))
     )
     exists((i : Int, j : Int) =>
-      (ArrayOps(a).isDefinedAt(i) && ArrayOps(a).isDefinedAt(j) && i != j) ==> (a(i) == a(j))
+      (ArrayOps(a).is_defined_at(i, j) && i != j) ==> (a(i) == a(j))
     )
   } holds /* verified */
 
   @induct
   def pigeonhole_principle_v2 (a : List[BigInt]) : Boolean = {
-    require(a.forall(ai => ai >= 0 && ai <= 1))
-    sum(a) <= a.size
+    require(a.forall(between(_, 0, 1)))
+    between(sum(a), 0, a.length)
   } holds /* verified */
 
   @induct
@@ -66,7 +64,8 @@ object PigeonHolePrinciples {
 object PigeonHolePrinciplesLemmas {
 
   case class ArrayOps[T] (a : Array[T]) {
-    def isDefinedAt (i : Int) = 0 <= i && i < a.length
+    def is_defined_at (i : Int) : Boolean = 0 <= i && i < a.length
+    def is_defined_at (i : Int, j : Int) : Boolean = is_defined_at(i) && is_defined_at(j)
   }
 
   /* Leon cannot handle implicit conversion with type parameter */
@@ -75,48 +74,22 @@ object PigeonHolePrinciplesLemmas {
   def exists[A] (p : A => Boolean) : Boolean = !forall((x : A) => !p(x))
   def exists[A, B] (p : (A, B) => Boolean) : Boolean = !forall((x : A, y : B) => !p(x, y))
 
-  def boundedBy (i : Int, lower : Int, upper : Int) = lower < i && i < upper
-  def boundedBy (i : BigInt, lower : BigInt, upper : BigInt) = lower < i && i < upper
-  def boundedBy (a : List[BigInt], lower : BigInt, upper : BigInt) : Boolean = {
-    a match {
-      case Nil()  => true
-      case h :: t => (lower < h && h < upper) && boundedBy(t, lower, upper)
-    }
+  def bounded_by (i : Int, lower : Int, upper : Int) = lower < i && i < upper
+  def bounded_by (i : BigInt, lower : BigInt, upper : BigInt) = lower < i && i < upper
+  def bounded_by (a : List[BigInt], lower : BigInt, upper : BigInt) : Boolean = {
+    a.forall(bounded_by(_, lower, upper))
   }
+  def between (i : BigInt, min : BigInt, max : BigInt) = min <= i && i <= max
 
-  def not_eq (a : List[BigInt], e : BigInt) : Boolean = {
-    a match {
-      case Nil()  => true
-      case h :: t => (h != e) && not_eq(t, e)
-    }
-  }
-
-  def eq (a : List[BigInt], e : BigInt) : Boolean = {
-    a match {
-      case Nil()  => true
-      case h :: t => (h == e) && eq(t, e)
-    }
-  }
-
-  /**
-    * We need to put this function in the post-condition of `count`,
-    * so that Leon can identify the result of `count` with the number
-    * of the counted elements in the provided list.
-    */
-  def sublist_eq (a : List[BigInt], e : BigInt) : List[BigInt] = {
-    a match {
-      case Nil()  => a
-      case h :: t => if (h == e) h :: sublist_eq(t, e) else sublist_eq(t, e)
-    }
-  }
-
-  def count (a : List[BigInt], e : BigInt) : BigInt = {
+  def frequency_of (a : List[BigInt], e : BigInt) : BigInt = {
     a match {
       case Nil()  => BigInt(0)
-      case h :: t => if (h == e) BigInt(1) + count(t, e) else BigInt(0) + count(t, e)
+      case h :: t => if (h == e) BigInt(1) + frequency_of(t, e) else BigInt(0) + frequency_of(t, e)
     }
   } ensuring { res =>
-    res == sublist_eq(a, e).length &&
+    /* The following assertion helps Leon identify the result of
+       frequency_of with the number of the counted elements in the list. */
+    res == a.filter(_ == e).length &&
       /* Without the above assertion, Leon can only prove (res > 0) ==> a.contains(e). */
       (res > 0) == a.contains(e)
   } /* verified */
@@ -129,64 +102,51 @@ object PigeonHolePrinciplesLemmas {
 
   @induct
   def can_loosen_upperbound (a : List[BigInt], lower : BigInt, upper : BigInt) : Boolean = {
-    require(boundedBy(a, 0, upper))
-    boundedBy(a, 0, upper + 1)
+    require(bounded_by(a, 0, upper))
+    bounded_by(a, 0, upper + 1)
   } holds /* verified */
 
   @induct
   def can_tighten_upperbound (a : List[BigInt], lower : BigInt, upper : BigInt) : Boolean = {
-    require(boundedBy(a, lower, upper) && not_eq(a, upper - 1))
-    boundedBy(a, lower, upper - 1)
+    require(bounded_by(a, lower, upper) && !a.contains(upper - 1))
+    bounded_by(a, lower, upper - 1)
   } holds
 
   @induct
-  def pigeon_lemma_2 (a : List[BigInt], n : BigInt) : Boolean = {
-    (boundedBy(a, 0, n) && not_eq(a, n - 1)) ==>
-      boundedBy(a, 0, n - 1)
-  } holds /* verified */
-
-  @induct
   def pigeon_lemma_1 (a : List[BigInt], n : BigInt) : Boolean = {
-    require(count(a, n) >= 2)
-    hasCollision(a)
+    require(frequency_of(a, n) >= 2)
+    has_collision(a)
   } holds /* verified */
 
   @induct
-  def pigeon_lemma_3 (a : List[BigInt], n : BigInt) : Boolean = {
-    require(count(a, n) == 0)
-    not_eq(a, n)
-  } holds /* verified */
-
-  @induct
-  def pigeon_lemma_4 (a : List[BigInt], n : BigInt) : Boolean = {
-    require(count(a, n - 1) == 1 && boundedBy(a, 0, n))
+  def pigeon_lemma_2 (a : List[BigInt], n : BigInt) : Boolean = {
+    require(frequency_of(a, n - 1) == BigInt(1) && bounded_by(a, 0, n))
     val a2 = a.removeFirst(n - 1)
-    boundedBy(a2, 0, n - 1) because {
+    bounded_by(a2, 0, n - 1) because {
       if (a.head != n - 1) {
         trivial
       } else {
-        pigeon_lemma_3(a2, n - 1) &&
-          can_tighten_upperbound(a2, 0, n)
+        can_tighten_upperbound(a2, 0, n)
       }
     }
   } holds /* verified */
 
   @induct
-  def pigeon_lemma_5 (a : List[BigInt], e : BigInt, n : BigInt) : Boolean = {
-    require(n == a.length && a.length >= 2 && boundedBy(a.removeFirst(e), 0, n - 1) &&
+  def pigeon_lemma_3 (a : List[BigInt], e : BigInt, n : BigInt) : Boolean = {
+    require(n == a.length && a.length >= 2 && bounded_by(a.removeFirst(e), 0, n - 1) &&
       a.contains(e) &&
-      boundedBy(e, 0, n) &&
-      pigeonhole_principle(a.removeFirst(e), n - 1))
-    pigeon_lemma_5_1(a, e, n) &&
-      pigeonhole_principle(a, n)
+      bounded_by(e, 0, n) &&
+      pigeonhole_principle(a.removeFirst(e)))
+    pigeon_lemma_3_1(a, e, n) &&
+      pigeonhole_principle(a)
   } holds /* verified */
 
   @induct
-  def pigeon_lemma_5_1 (a : List[BigInt], e : BigInt, n : BigInt) : Boolean = {
-    require(boundedBy(a.removeFirst(e), 0, n - 1) &&
+  def pigeon_lemma_3_1 (a : List[BigInt], e : BigInt, n : BigInt) : Boolean = {
+    require(bounded_by(a.removeFirst(e), 0, n - 1) &&
       a.contains(e) &&
-      0 < e && e < n)
-    boundedBy(a, 0, n) because {
+      bounded_by(e, 0, n))
+    bounded_by(a, 0, n) because {
       if (a.head != e) {
         trivial
       } else {
